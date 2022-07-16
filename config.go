a6cc5937bf0abb878b7012a6126fca92b397e6a8
rnascimento <rnascimento@godaddy.com>
2022-07-16 11:13:08 -0300

    New itens added

--- config.go
+++ config.go
@@ -1,13 +1,3 @@
 package poctools
 
-type DatabaseSettings struct {
-	DbClusterIdentifier string `json:"dbClusterIdentifier"`
-	Password            string `json:"password"`
-	Engine              string `json:"engine"`
-	Port                int    `json:"port"`
-	Host                string `json:"host"`
-	Username            string `json:"username"`
-	DatabaseName        string
-}
-
 var DefaultPaginationLimit int64
